c00217c66dd16b10eae52394314514b8f06bc7a2
unicodesnowman <utfsnowman@gmail.com>
2015-08-09 11:16:02 -0400

    using pm2, it supports babel transpilation, get rid of require-hook

--- src/server/index.js
+++ src/server/index.js
@@ -1,2 +1,24 @@
-require('babel/register');
-require('./server');
+import express from 'express';
+import React from 'react';
+import Router from 'react-router';
+import routes from '../routes';
+
+const app = express();
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'jade');
+app.use(express.static('public'));
+app.use(express.static('src/assets'));
+
+app.get('/*', (req, res) => {
+    Router.run(routes, req.url, Handler => {
+        res.render('index', {
+            content: React.renderToString(<Handler />)
+        });
+    });
+});
+
+var server = app.listen(8080, () => {
+    var { host, port } = server.address();
+    console.log(`Listening on ${port}...`);
+});
